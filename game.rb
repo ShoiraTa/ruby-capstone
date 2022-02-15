82d6a4c378a80ead3d4b3d38649f4442f9dfbd53
Munsa1 <81932478+Munsa1@users.noreply.github.com>
2022-02-15 15:25:19 +0200

    Add can_be_archived  method to game class

--- game.rb
+++ game.rb
@@ -5,4 +5,9 @@ class Game
     @last_played_date = Date.new(last_played_date)
     @multiplayer = multiplayer
   end
+
+  def can_be_archived?
+    duration = Time.now.year - @last_played_date.year
+    super && duration > 2
+  end
 end
