e394848af0ec4f42c1088b49316257ef5e25321b
Munsa1 <81932478+Munsa1@users.noreply.github.com>
2022-02-15 15:14:48 +0200

    Add initialize method to game class

--- game.rb
+++ game.rb
@@ -1,2 +1,8 @@
+require_relative './item'
 class Game
+  def initialize(publish_date, multiplayer, last_played_date)
+    super(publish_date)
+    @last_played_date = Date.new(last_played_date)
+    @multiplayer = multiplayer
+  end
 end
